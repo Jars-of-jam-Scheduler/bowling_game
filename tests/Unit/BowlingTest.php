70b34348ffae7212519bd7b0ba80c6f1f7916b02
Jars <anon>
2022-09-04 14:09:49 +0200

    Renamed variable instance BowlingClass in test class.

--- tests/Unit/BowlingTest.php
+++ tests/Unit/BowlingTest.php
@@ -16,72 +16,72 @@ class BowlingTest extends TestCase
 
     public function testOnlyGutters()
     {
-		$bowling_class = new BowlingClass;
-		$bowling_class->generateSeveralRounds(10, 0, 0);
-        $this->assertEquals(0, $bowling_class->getScore());
+		$bowling_instance = new BowlingClass;
+		$bowling_instance->generateSeveralRounds(10, 0, 0);
+        $this->assertEquals(0, $bowling_instance->getScore());
     }
 
 	public function testOnlyStrikes()
     {
-		$bowling_class = new BowlingClass;
-		$bowling_class->generateSeveralRounds(10, 10);
-		$bowling_class->generateBonusRows(10, 10);
-        $this->assertEquals(300, $bowling_class->getScore());
+		$bowling_instance = new BowlingClass;
+		$bowling_instance->generateSeveralRounds(10, 10);
+		$bowling_instance->generateBonusRows(10, 10);
+        $this->assertEquals(300, $bowling_instance->getScore());
     }
 
 	public function testOnlySpares()
     {
-		$bowling_class = new BowlingClass;
-		$bowling_class->generateSeveralRounds(10, 9, 1);
-		$bowling_class->generateBonusRows(10);
-        $this->assertEquals(191, $bowling_class->getScore());
+		$bowling_instance = new BowlingClass;
+		$bowling_instance->generateSeveralRounds(10, 9, 1);
+		$bowling_instance->generateBonusRows(10);
+        $this->assertEquals(191, $bowling_instance->getScore());
     }
 
 	public function test1Strike()
     {
-		$bowling_class = new BowlingClass;
-		$bowling_class->generateSeveralRounds(1, 10);
-		$bowling_class->generateSeveralRounds(9, 0, 0);
-        $this->assertEquals(10, $bowling_class->getScore());
+		$bowling_instance = new BowlingClass;
+		$bowling_instance->generateSeveralRounds(1, 10);
+		$bowling_instance->generateSeveralRounds(9, 0, 0);
+        $this->assertEquals(10, $bowling_instance->getScore());
     }
 
 	public function test1Spare()
     {
-		$bowling_class = new BowlingClass;
-		$bowling_class->generateSeveralRounds(1, 9, 1);
-		$bowling_class->generateSeveralRounds(9, 0, 0);
-        $this->assertEquals(10, $bowling_class->getScore());
+		$bowling_instance = new BowlingClass;
+		$bowling_instance->generateSeveralRounds(1, 9, 1);
+		$bowling_instance->generateSeveralRounds(9, 0, 0);
+        $this->assertEquals(10, $bowling_instance->getScore());
     }
 
 	public function testCombination1()
     {
-		$bowling_class = new BowlingClass;
-		$bowling_class->generateSeveralRounds(1, 10);
-		$bowling_class->generateSeveralRounds(1, 5, 5);
-		$bowling_class->generateSeveralRounds(8, 0, 0);
-        $this->assertEquals(30, $bowling_class->getScore());
+		$bowling_instance = new BowlingClass;
+		$bowling_instance->generateSeveralRounds(1, 10);
+		$bowling_instance->generateSeveralRounds(1, 5, 5);
+		$bowling_instance->generateSeveralRounds(8, 0, 0);
+        $this->assertEquals(30, $bowling_instance->getScore());
     }
 
 	public function testCombination2()
     {
-		$bowling_class = new BowlingClass;
-		$bowling_class->generateSeveralRounds(1, 10);
-		$bowling_class->generateSeveralRounds(8, 0, 0);
-		$bowling_class->generateSeveralRounds(1, 5, 5);
-		$bowling_class->generateBonusRows(10);
-        $this->assertEquals(30, $bowling_class->getScore());
+		$bowling_instance = new BowlingClass;
+		$bowling_instance->generateSeveralRounds(1, 10);
+		$bowling_instance->generateSeveralRounds(8, 0, 0);
+		$bowling_instance->generateSeveralRounds(1, 5, 5);
+		$bowling_instance->generateBonusRows(10);
+        $this->assertEquals(30, $bowling_instance->getScore());
     }
 
 	public function testCombination3()
     {
-		$bowling_class = new BowlingClass;
-		$bowling_class->generateSeveralRounds(2, 10);
-		$bowling_class->generateSeveralRounds(2, 5, 5);
-		$bowling_class->generateSeveralRounds(3, 10);
-		$bowling_class->generateSeveralRounds(2, 0, 0);
-		$bowling_class->generateSeveralRounds(1, 5, 5);
-		$bowling_class->generateBonusRows(10);
-        $this->assertEquals(160, $bowling_class->getScore());
+		$bowling_instance = new BowlingClass;
+		$bowling_instance->generateSeveralRounds(2, 10);
+		$bowling_instance->generateSeveralRounds(2, 5, 5);
+		$bowling_instance->generateSeveralRounds(3, 10);
+		$bowling_instance->generateSeveralRounds(2, 0, 0);
+		$bowling_instance->generateSeveralRounds(1, 5, 5);
+		$bowling_instance->generateBonusRows(10);
+        $this->assertEquals(160, $bowling_instance->getScore());
     }
 
 }
